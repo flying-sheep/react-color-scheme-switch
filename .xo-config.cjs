d9eca90b34d545287c2a367541ae1440a7cba917
Philipp A <flying-sheep@web.de>
2021-11-09 15:57:21 +0100

    Fix lints

---
+++ .xo-config.cjs
@@ -0,0 +1,20 @@
+const { rules } = require('eslint-config-xo-typescript')
+
+const NC = '@typescript-eslint/naming-convention'
+
+const nameRules = rules[NC].map((r) => {
+  if (typeof r === 'string') return r
+  const { selector, format, ...rest } = r
+  if (!selector.includes('variable') || !selector.includes('function')) return r
+  return { selector, format: ['strictCamelCase', 'StrictPascalCase'], ...rest }
+})
+
+module.exports = {
+  semicolon: false,
+  extends: [
+    'xo-react',
+  ],
+  rules: {
+    [NC]: nameRules,
+  }
+}
