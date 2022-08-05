dfb63305b77964cf3883723c9871e27e69dcfff7
Philipp A <flying-sheep@web.de>
2022-08-05 15:13:28 +0200

    WIP

--- scripts/compile-docs.ts
+++ scripts/compile-docs.ts
@@ -4,7 +4,10 @@ import {compile} from 'xdm'
 void main()
 
 async function main() {
-	const compiled = await compile(await fs.readFile('./src/index.mdx'))
-	await fs.mkdir('./docs')
-	await fs.writeFile('./docs/index.js', String(compiled))
+	const compiled = await compile(await fs.readFile('./src/docs/index.mdx'))
+	await fs.mkdir('./dist/docs', {recursive: true})
+	await Promise.all([
+		fs.writeFile('./dist/docs/index.js', String(compiled)),
+		fs.copyFile('./src/docs/index.html', './dist/docs/index.html'),
+	])
 }
